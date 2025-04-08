22aebd05f366bdbe15cfcb16ac94e0e6b17d440a
William E. Skeith <wes@cs.ccny.cuny.edu>
2025-04-08 12:46:18 -0400

    Notes from 18th meeting.

---
+++ 18/lists.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+struct node {
+	int data;
+	node* next;
+};
+
+int main()
+{
+	node* L = NULL;
+	/* TODO: try to actually write the code from class today to add new
+	 * elements to the beginning of a list and to print the contents of
+	 * a list.  */
+}
+
+// vim:foldlevel=2
